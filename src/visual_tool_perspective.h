b2b14bb48c608b28c0ced9ccae6b6c834d8b1535
arch1t3cht <arch1t3cht@gmail.com>
2025-12-18 14:30:09 +0100

    Support LayoutRes in perspective tool

--- src/visual_tool_perspective.h
+++ src/visual_tool_perspective.h
@@ -94,6 +94,8 @@ class VisualToolPerspective final : public VisualTool<VisualToolPerspectiveDragg
 	std::vector<Feature *> inner_corners;
 	std::vector<Feature *> outer_corners;
 
+	float screenZ() const;
+
 	std::vector<Vector2D> FeaturePositions(std::vector<Feature *> features) const;
     void UpdateInner();
     void UpdateOuter();
