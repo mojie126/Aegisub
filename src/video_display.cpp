004ff49ccaced2febd8f9b284bc49720e312d3ce
arch1t3cht <arch1t3cht@gmail.com>
2026-02-01 14:18:36 +0100

    Use new API for building wxGLAttributes

--- src/video_display.cpp
+++ src/video_display.cpp
@@ -67,8 +67,16 @@
 #include <GL/glcorearb.h>
 #endif
 
+namespace {
+
 /// Attribute list for gl canvases; set the canvases to doublebuffered rgba with an 8 bit stencil buffer
-int attribList[] = { WX_GL_RGBA , WX_GL_DOUBLEBUFFER, WX_GL_STENCIL_SIZE, 8, 0 };
+wxGLAttributes buildGLAttributes() {
+	wxGLAttributes attrs;
+	attrs.RGBA().DoubleBuffer().Stencil(8).EndList();
+	return attrs;
+}
+
+}
 
 /// An OpenGL error occurred while uploading or displaying a frame
 class OpenGlException final : public agi::Exception {
@@ -93,7 +101,7 @@ int GetFFMSPaddingPixels(AsyncVideoProvider *provider) {
 #define E(cmd) cmd; if (GLenum err = glGetError()) throw OpenGlException(#cmd, err)
 
 VideoDisplay::VideoDisplay(wxToolBar *toolbar, bool freeSize, wxComboBox *zoomBox, wxWindow *parent, agi::Context *c)
-: wxGLCanvas(parent, -1, attribList)
+: wxGLCanvas(parent, buildGLAttributes())
 , autohideTools(OPT_GET("Tool/Visual/Autohide"))
 , con(c)
 , windowZoomValue(OPT_GET("Video/Default Zoom")->GetInt() * .125 + .125)
