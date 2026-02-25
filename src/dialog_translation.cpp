dc6fa0ed6a2b21c76b339790a748331df8bb827c
mojie126 <mojie126@foxmail.com>
2026-02-25 19:54:14 +0800

    Fix deprecated wxWidgets usages

--- src/dialog_translation.cpp
+++ src/dialog_translation.cpp
@@ -249,11 +249,7 @@ void DialogTranslation::UpdateDisplay() {
 			int initial_pos = original_text->GetLength();
 			original_text->AppendTextRaw(block->GetText().c_str());
 			if (i == cur_block) {
-#if wxVERSION_NUMBER >= 3100
 				original_text->StartStyling(initial_pos);
-#else
-				original_text->StartStyling(initial_pos, 255);
-#endif
 				original_text->SetStyling(block->GetText().size(), 1);
 			}
 		}
