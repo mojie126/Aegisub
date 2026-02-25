e3ac6b3cfc995234e15e57ceaf7e0bd80614d61c
mojie126 <mojie126@foxmail.com>
2026-02-25 19:54:26 +0800

    Allow translating choices for string options in preferences

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -494,9 +494,9 @@ void Advanced_Video(wxTreebook *book, Preferences *parent) {
 #ifdef WITH_FFMS2
 	auto ffms = p->PageSizer("FFmpegSource");
 
-	const wxString log_levels[] = { _("Quiet"), _("Panic"), _("Fatal"), _("Error"), _("Warning"), _("Info"), _("Verbose"), _("Debug") };
+	const wxString log_levels[] = { wxTRANSLATE("Quiet"), wxTRANSLATE("Panic"), wxTRANSLATE("Fatal"), wxTRANSLATE("Error"), wxTRANSLATE("Warning"), wxTRANSLATE("Info"), wxTRANSLATE("Verbose"), wxTRANSLATE("Debug") };
 	wxArrayString log_levels_choice(8, log_levels);
-	p->OptionChoice(ffms, _("Debug log verbosity"), log_levels_choice, "Provider/FFmpegSource/Log Level");
+	p->OptionChoice(ffms, _("Debug log verbosity"), log_levels_choice, "Provider/FFmpegSource/Log Level", true);
 
 	// 添加黑边
 	p->OptionAdd(ffms, _("Add black borders"), "Provider/Video/FFmpegSource/ABB", 0)
